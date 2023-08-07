7bc698438cb923c948a2b58a6660acb2c4a7c8af
RicardoJavGF <RicardoJavGF@gmail.com>
2023-08-07 06:58:03 -0400

    agregando funcionalidad JS

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function showModal() {
+    document.getElementById('modal').style.display = 'flex';
+  }
+
+  // Function to hide the modal
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+  }
